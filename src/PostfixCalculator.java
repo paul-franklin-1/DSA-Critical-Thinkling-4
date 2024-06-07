12285f8240e68c9f4510a3e46a8997dd89c11e87
Paul Franklin <paul.franklin@csuglobal.edu>
2024-06-07 19:38:59 -0400

    first save of CT4/DSA

---
+++ src/PostfixCalculator.java
@@ -0,0 +1,9 @@
+public class PostfixCalculator {
+    public int evaluatePostfix(String postfixExpression){
+
+    }
+    public static void main(String[] args){
+        PostfixCalculator calculator = new PostfixCalculator();
+
+    }
+}
